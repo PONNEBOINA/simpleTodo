a53b86642b6c4cb2e4999a9b96ab62fc16c27d9a
PONNEBOINA <vamshiyadav406@gmail.com>
2024-10-30 09:06:30 +0000

    todo

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+// import {v4 as uuidv4} from 'uuid'
 
 import TodoItem from '../TodoItem'
 
@@ -6,39 +7,47 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    complete: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    complete: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    complete: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    complete: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    complete: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    complete: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    complete: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    complete: false,
   },
 ]
 
 class SimpleTodos extends Component {
-  state = {todolist: initialTodosList}
+  state = {todolist: initialTodosList, newTodo: ''}
 
   ondeletetodo = id => {
     const {todolist} = this.state
@@ -46,17 +55,48 @@ class SimpleTodos extends Component {
     this.setState({todolist: updatetodolist})
   }
 
+  onChangeNew = e => {
+    this.setState({newTodo: e.target.value})
+  }
+
+  addNewTodo = () => {
+    const {newTodo} = this.state
+    const newTitle = {
+      // id : uuidv4(),
+      title: newTodo,
+      complete: false,
+    }
+    this.setState(prevState => ({
+      todolist: [...prevState.todolist, newTitle],
+      newTodo: '',
+    }))
+  }
+
+  togglecomplete = id => {
+    this.setState(prevState => {
+      const updatetodo = prevState.todolist.map(todo =>
+        todo.id === id ? {...todo, complete: !todo.complete} : todo,
+      )
+      return {todolist: updatetodo}
+    })
+  }
+
   render() {
-    const {todolist} = this.state
+    const {todolist, newTodo} = this.state
     return (
       <div>
         <h1>Simple Todos</h1>
+        <input type="text" onChange={this.onChangeNew} value={newTodo} />
+        <button type="button" onClick={this.addNewTodo}>
+          Add
+        </button>
         <ul>
           {todolist.map(eachtodo => (
             <TodoItem
               key={eachtodo.id}
               tododetails={eachtodo}
               deletetodo={this.ondeletetodo}
+              togglecomplete={this.togglecomplete}
             />
           ))}
         </ul>
@@ -65,3 +105,9 @@ class SimpleTodos extends Component {
   }
 }
 export default SimpleTodos
+
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PONNEBOINA/EnhancementOfRestaurant.git
+// git push -u origin main
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,20 +1,73 @@
 // Write your code here
+import {Component} from 'react'
 
-const TodoItem = props => {
-  const {tododetails, deletetodo} = props
-  const {id, title} = tododetails
-  const ondeletetodo = () => {
-    deletetodo(id)
+class TodoItem extends Component {
+  state = {editTitle: false, updateTitle: ''}
+
+  onEdit = () => {
+    const {tododetails} = this.props
+    this.setState({editTitle: true, updateTitle: tododetails.title})
   }
-  return (
-    <li>
-      <p>{title}</p>
-      <button type="button" onClick={ondeletetodo}>
-        Delete
-      </button>
-    </li>
-  )
+
+  handleChange = e => {
+    this.setState({updateTitle: e.target.value})
+  }
+
+  saveBtn = () => {
+    this.setState({editTitle: false})
+  }
+
+  render() {
+    const {editTitle, updateTitle} = this.state
+    const {tododetails, deletetodo, togglecomplete} = this.props
+    const {id, title, complete} = tododetails
+
+    const deleted = () => {
+      deletetodo(id)
+    }
+
+    return (
+      <li>
+        <>
+          {editTitle ? (
+            <>
+              <input
+                type="text"
+                value={updateTitle}
+                onChange={this.handleChange}
+              />
+              <button type="button" onClick={this.saveBtn}>
+                Save
+              </button>
+            </>
+          ) : (
+            <>
+              <input
+                type="checkbox"
+                onChange={() => togglecomplete(tododetails.id)}
+              />
+              {complete ? (
+                <p
+                  style={{
+                    textDecoration: 'line-through',
+                  }}
+                >
+                  {title}
+                </p>
+              ) : (
+                <p>{title}</p>
+              )}
+              <button type="button" onClick={this.onEdit}>
+                Edit
+              </button>
+              <button type="button" onClick={deleted}>
+                Delete
+              </button>
+            </>
+          )}
+        </>
+      </li>
+    )
+  }
 }
-
 export default TodoItem
-
